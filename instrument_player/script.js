e73a3eb6830146187a6fa838faa4f82f8e106f20
Praveen Kispotta <pkumarkispotta@gmail.com>
2024-03-23 22:47:42 +0530

    update

--- instrument_player/script.js
+++ instrument_player/script.js
@@ -1,57 +1,51 @@
-let buttonInnerHTML;
-let numberOfDrumButtons = document.querySelectorAll(".drum").length;
-for (let i = 0; i < numberOfDrumButtons; i++){
-document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-buttonInnerHTML = this.innerHTML;
-makeSound(buttonInnerHTML);
-});
+// Create eventListener to all the button 
+// All button should be able to play a different sound.
+const track = document.querySelectorAll(".drum");
+for(let i = 0; i<track.length; i++){
+    track[i].addEventListener("click", ()=>{
+        makeSound(track[i].innerHTML);
+    })
 }
 
-//detecting keyboard press
-document.addEventListener("keypress", function(event) {
-makeSound(event.key);
-});
+document.addEventListener("keypress", (event)=>{
+    makeSound(event.key);
+})
 
 function makeSound(key){
-
-  switch (key) {
-    case "w":
-    var tom1 = new Audio("https://files.codingninjas.in/tom-1-28537.mp3");
-    tom1.play();
-      break;
-
-      case "a":
-      var tom2 = new Audio("https://files.codingninjas.in/tom-2-28541.mp3");
-      tom2.play();
-      break;
-
-      case "s":
-      var tom3 = new Audio("https://files.codingninjas.in/tom-3-28542.mp3");
-      tom3.play();
-      break;
-
-      case "d":
-      var tom4 = new Audio("https://files.codingninjas.in/tom-4-28543.mp3");
-      tom4.play();
-      break;
-
-      case "j":
-      var snare = new Audio("https://files.codingninjas.in/snare-28545.mp3");
-      snare.play();
-      break;
-
-      case "k":
-      var crash = new Audio("https://files.codingninjas.in/crash-28546.mp3");
-      crash.play();
-      break;
-
-      case "l":
-      var kick = new Audio("https://files.codingninjas.in/kick-bass-28547.mp3");
-      kick.play();
-      break;
-      default: 
-      console.log(buttonInnerHTML);
-
-  }
-
+    switch(key){
+        case 'w':
+            let track1 = new Audio("https://files.codingninjas.in/tom-1-28537.mp3");
+            track1.play();
+            break;
+            
+        case 'a':
+            let track2 = new Audio("https://files.codingninjas.in/tom-2-28541.mp3");
+            track2.play();
+            break;
+            
+            case 's':
+            let track3 = new Audio("https://files.codingninjas.in/tom-3-28542.mp3");
+            track3.play();
+            break;
+            
+        case 'd':
+            let track4 = new Audio("https://files.codingninjas.in/tom-4-28543.mp3");
+            track4.play();
+            break;
+            
+            case 'j':
+            let track5 = new Audio("https://files.codingninjas.in/snare-28545.mp3");
+            track5.play();
+            break;
+            
+        case 'k':
+            let track6 = new Audio("https://files.codingninjas.in/crash-28546.mp3");
+            track6.play();
+            break;
+            
+            case 'l':
+            let track7 = new Audio("https://files.codingninjas.in/kick-bass-28547.mp3");
+            track7.play();
+            break;
+    }
 }
