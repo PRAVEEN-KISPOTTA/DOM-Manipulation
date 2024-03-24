5f2fbf3bb0c4e40fd0d1610b9d651b432e3ae337
Praveen Kispotta <pkumarkispotta@gmail.com>
2024-03-24 23:51:47 +0530

    dropDown menu added

--- seat_book_app/script.js
+++ seat_book_app/script.js
@@ -5,6 +5,14 @@ const moviesList = [
     { movieName: "Batman", price: 4 },
   ];
 // Use moviesList array for displaing the Name in the dropdown menu
+const dropDownList = document.querySelector("#selectMovie");
+
+moviesList.forEach(movie=>{
+    const option = document.createElement("option");
+    option.innerHTML = movie.movieName
+    option.value = movie.movieName;
+    dropDownList.appendChild(option);
+})
 //Add eventLister to each unoccupied seat
 //Add eventLsiter to continue Button
 //Add eventListerner to Cancel Button
