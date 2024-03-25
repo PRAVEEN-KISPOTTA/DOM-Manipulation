251134d94bdd1cd8e6d045c5cb8cb96f9c5ca353
Praveen Kispotta <pkumarkispotta@gmail.com>
2024-03-26 02:29:14 +0530

    seats occupied and unOccupied problem solved

--- seat_book_app/script.js
+++ seat_book_app/script.js
@@ -6,13 +6,43 @@ const moviesList = [
   ];
 // Use moviesList array for displaing the Name in the dropdown menu
 const dropDownList = document.querySelector("#selectMovie");
+const movieNameUpdate = document.querySelector("#movieName");
+const moviePriceUpdate = document.querySelector("#moviePrice");
 
 moviesList.forEach(movie=>{
     const option = document.createElement("option");
-    option.innerHTML = movie.movieName
+    option.innerHTML = movie.movieName + " $"+ movie.price
     option.value = movie.movieName;
     dropDownList.appendChild(option);
-})
+});
+
+console.log(dropDownList[2].value)
+dropDownList.addEventListener("change", ()=>{
+    const selectMovieName = dropDownList.value;
+    console.log(selectMovieName);
+    const selectedMovie = moviesList.find((movie)=>movie.movieName === selectMovieName);
+
+    if(selectedMovie){
+        movieNameUpdate.textContent = selectedMovie.movieName;
+        moviePriceUpdate.textContent = "$ " + selectedMovie.price;
+    }
+
+    console.log(selectedMovie);
+});
 //Add eventLister to each unoccupied seat
+const seatCont = document.querySelectorAll("#seatCont .seat");
+const unOccupied = Array.from(seatCont).filter(seat=> !seat.classList.contains("occupied"));
+console.log(unOccupied)
+
+unOccupied.forEach(seats=>{
+    seats.addEventListener("click", ()=>{
+        if(!seats.classList.contains("selected")){
+            seats.classList.add("selected")
+        }
+        else{
+            seats.classList.remove("selected");
+        }
+    })
+})
 //Add eventLsiter to continue Button
 //Add eventListerner to Cancel Button
